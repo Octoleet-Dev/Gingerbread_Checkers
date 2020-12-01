34da2fcec75e624221612d8893cc2e585ca20408
Otaku-samaIII <69971976+Octoleet-Dev@users.noreply.github.com>
2020-12-01 09:45:55 +0000

    Main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+print("under apache liscence 2.0")
+print("a physical board is required to play this game")
+tsncs = input("Press 1 to accept these conditions: ")
+accepted = 1
+declined = 0
+thetsncs = int(tsncs)
+if thetsncs == accepted:
+print('Thank you!')
+print("The software isn't open source right now, please email leokeene0@gmail.com for access to the beta")
+print("If you have a beta liscence, please enter it here")
+Alex's_beta_liscence = 88224646ba
+liscence = input('>>>')
+else:
+print("error code: 20")
+print("Please restart the software and start again")
